5ac4e87ac8bf5cbd42494de54ab76d24bfd5c309
lilia <lilia.campo@ensg.eu>
2023-11-20 20:13:24 +0100

    coord des objets

--- carte.js
+++ carte.js
@@ -1,7 +1,16 @@
-var zoom_min = 19;
+var zoom_min = 16;
 var zoom_max = 22;
 
-var map = L.map('map', {minZoom : zoom_min}).setView([35.70754798478091, 139.76373632159613],19);
+var map = L.map('map', {minZoom : zoom_min}).setView([35.707529564411864, 139.76302385330203],19);
+
+var livre = L.marker([35.707385818127044, 139.7617853432894]).addTo(map);
+var monsieur_indic = L.marker([35.707570945870046, 139.76315528154376]).addTo(map);
+var alliance = L.marker([35.71060480162236, 139.76841241121295]).addTo(map);
+var coord_hotel = L.marker([35.711701088466086, 139.7685139998794]).addTo(map);
+var couteau = L.marker([35.713119133684295, 139.75828103721142]).addTo(map);
+var mari = L.marker([35.71327920324335, 139.75836485624316]).addTo(map);
+var journal = L.marker([35.7133227595303, 139.7581543028355]).addTo(map);
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: zoom_max,
